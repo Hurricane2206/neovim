a0eb2ee03c7a723905d7b19746785e443fd8d1a8
Hurricane2206 <hurricane2206@proton.me>
2025-01-10 01:40:22 -0800

    added rasi to tsconfig

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 pts = {
 		ensure_installed = {
-		"vim", "lua", "vimdoc", "html", "css", "c", "cpp", "rust", "toml", "yaml", "bash", "c_sharp", "python"
+		"vim", "lua", "vimdoc", "html", "css", "c", "cpp", "rust", "toml", "yaml", "bash", "c_sharp", "python", "rasi"
 		},
 	},
 }
