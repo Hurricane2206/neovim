98e924202bcd3f64c6e6d8c8e58ad30d99bd97a0
Hurricane2206 <hurricane2206@proton.me>
2025-04-07 01:28:16 -0700

    added cmake to tresitter

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -7,11 +7,11 @@ local options = {
     cpp = { "clangd_format"}
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
