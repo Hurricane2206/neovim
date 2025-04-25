b99098886028dfa8fea5a6a46aed1963e430f99f
Hurricane2206 <hurricane2206@proton.me>
2025-04-25 15:30:30 -0700

    added nvim-dap
    does not function

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,29 +1,16 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
   {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "clangd", "rust-analyzer"
+        "clangd",
+        "rust-analyzer",
       },
     },
   },
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "theHamsta/nvim-dap-virtual-text",
-      opts = {},
-    },
-  },
-  {
-    "julianolf/nvim-dap-lldb",
-    opts = {
-      codelldb_path = "./lldb/codelldb-linux-x64.vsix",
-    },
-  }
 }
